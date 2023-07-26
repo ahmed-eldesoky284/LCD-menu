8608ec42985d9e0c42aa41c08fdbb5f4ddbcda8d
Ahmed Eldesoky <66114762+ahmed-eldesoky284@users.noreply.github.com>
2023-07-26 20:38:38 +0300

    Update lcd_menu1.ino

--- lcd_menu1.ino
+++ lcd_menu1.ino
@@ -3,12 +3,23 @@ LiquidCrystal_I2C mylcd(32,16,2);
 byte up=3;
 byte down=2;
 int menu=1;
+byte Arrow[] = {
+  B01000,
+  B01100,
+  B01110,
+  B01111,
+  B01110,
+  B01100,
+  B01000,
+  B00000
+};
 
 void setup(){
   pinMode(up,INPUT_PULLUP);
   pinMode(down,INPUT_PULLUP);
   mylcd.init();
   mylcd.backlight();
+  mylcd.createChar(0,Arrow);
   updateMenu();
 
 }
@@ -36,19 +47,28 @@ void updateMenu(){
     menu =1;
   case 1:
     mylcd.clear();
-    mylcd.print("> Selcet Program");
-    mylcd.setCursor(0,1);
-    mylcd.print(" Setting");
+    mylcd.setCursor(0,0);
+    mylcd.write(0);
+    mylcd.setCursor(1,0);
+    mylcd.print("Selcet Program");
+    mylcd.setCursor(1,1);
+    mylcd.print("Setting");
     break;
   case 2:
     mylcd.clear();
+    mylcd.setCursor(0,0);
     mylcd.print(" Selcet Program");
     mylcd.setCursor(0,1);
-    mylcd.print("> Setting");
+    mylcd.write(0);
+    mylcd.setCursor(1,1);
+    mylcd.print(" Setting");
     break;
   case 3:
     mylcd.clear();
-    mylcd.print("> Powersaving");
+    mylcd.setCursor(0,0);
+    mylcd.write(0);
+    mylcd.setCursor(1,0);
+    mylcd.print(" Powersaving");
     mylcd.setCursor(0,1);
     mylcd.print(" Shutdown");
     break;
@@ -56,7 +76,9 @@ void updateMenu(){
     mylcd.clear();
     mylcd.print(" Powersaving");
     mylcd.setCursor(0,1);
-    mylcd.print("> Shutdown");
+    mylcd.write(0);
+    mylcd.setCursor(1,1);
+    mylcd.print(" Shutdown");
     break;
   
   }
